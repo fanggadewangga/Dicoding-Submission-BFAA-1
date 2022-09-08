ad1877029e42a6c1ba4b99155bc2a5a6b9dcd63a
fanggadewangga <fanggadewangga@gmail.com>
2022-09-08 21:02:36 +0700

    initial commit

---
+++ app/src/main/java/com/fangga/bfaa/githubuserapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.fangga.bfaa.githubuserapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.fangga.bfaa.githubuserapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
